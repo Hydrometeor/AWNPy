fd0f477eb802ed2adfb2206656ac58ebedded376
joejoezz <joejoezz@gmail.com>
2020-05-28 17:44:16 -0700

    Converted Metadata function to AWN compatibility

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='MesoPy',
-    py_modules=['MesoPy'],
-    version='2.0.3',
-    description='A Python wrapper for the MesoWest environmental data',
-    author='Mesowest/SynopticLabs',
-    author_email='help@synopticlabs.org',
-    url='https://github.com/mesowx/MesoPy',
-    download_url='https://github.com/mesowx/MesoPy/tarball/2.0.2',
-    keywords=['weather API climate MesoWest meteorology'],
+    name='AWNPy',
+    py_modules=['AWNPy'],
+    version='0.0.1',
+    description='A Python wrapper for AgWeatherNet weather data, based on MesoPy by Synoptic Labs',
+    author='joejoezz',
+    author_email='joe.zagrodnik@wsu.edu',
+    url='https://github.com/joejoezz/AWNPy',
+    #download_url='https://github.com/mesowx/MesoPy/tarball/2.0.2',
+    keywords=['weather API climate AgWeatherNet meteorology'],
     classifiers=[],
     requires=[],
 )
